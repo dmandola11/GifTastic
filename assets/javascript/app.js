de219a5744d06603fe41ec3fdaea939bb63cf10e
Damian Mandola Jr <damianjr@Damian-Mandola-Jrs-MacBook-Pro.local>
2016-09-09 01:22:02 -0500

    buttons display dynamically from array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,16 @@
+$(document).ready(function() {
 
+            var food = ["pizza", "cheeseburger", "cereal", "bacon", "cupcake", "fries", "pasta",
+             "curry", "sushi", "bread", "gelato", "sandwich"];
 
-var food = [pizza, cheeseburger, cereal, bacon, cupcake, fries, pasta, curry, sushi, bread, gelato, sandwich]
+            function addButtons(){
+            for (var i = 0; i < food.length; i++) {
+
+                var displayButtons = $('<button type ="button">' + food[i] + '</button>');
+                console.log(displayButtons);
+                $('#foodButtons').append(displayButtons);
+
+                }
+                }
+                addButtons();
+            });
